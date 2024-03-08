01b0192f9bf830e982b62d811b66b1460c4cea1b
Tobiloba <salauoluwatobiloba0@gmail.com>
2024-03-08 10:21:07 +0100

    fixed eslint issue

--- src/components/testing/src/common/RoundedButton/index.jsx
+++ src/components/testing/src/common/RoundedButton/index.jsx
@@ -1,39 +1,88 @@
-import React from 'react'
-import { useEffect, useRef } from 'react';
+// import React from 'react'
+// import { useEffect, useRef } from 'react';
+// import styles from './style.module.scss';
+// import gsap from 'gsap';
+// import Magnetic from '../Magnetic';
+
+// export default function index({children, backgroundColor="#fd7e14", ...attributes}) {
+
+//   const circle = useRef(null);
+//   let timeline = useRef(null);
+//   let timeoutId = null;
+//   useEffect( () => {
+//     timeline.current = gsap.timeline({paused: true})
+//     timeline.current
+//       .to(circle.current, {top: "-25%", width: "150%", duration: 0.4, ease: "power3.in"}, "enter")
+//       .to(circle.current, {top: "-150%", width: "125%", duration: 0.25}, "exit")
+//   }, [])
+  
+//   const manageMouseEnter = () => {
+//     if(timeoutId) clearTimeout(timeoutId)
+//     timeline.current.tweenFromTo('enter', 'exit');
+//   }
+
+//   const manageMouseLeave = () => {
+//     timeoutId = setTimeout( () => {
+//       timeline.current.play();
+//     }, 300)
+//   }
+
+//   return (
+//     <Magnetic>
+//       <div className={styles.roundedButton} style={{overflow: "hidden"}} onMouseEnter={() => {manageMouseEnter()}} onMouseLeave={() => {manageMouseLeave()}} {...attributes}>
+//           {
+//             children
+//           }
+//         <div ref={circle} style={{backgroundColor}} className={styles.circle}></div>
+//       </div>
+//     </Magnetic>
+//   )
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+import React, { useEffect, useRef } from 'react';
 import styles from './style.module.scss';
 import gsap from 'gsap';
 import Magnetic from '../Magnetic';
 
-export default function index({children, backgroundColor="#fd7e14", ...attributes}) {
-
+export default function RoundedButton({ children, backgroundColor = "#fd7e14", ...attributes }) {
   const circle = useRef(null);
   let timeline = useRef(null);
   let timeoutId = null;
-  useEffect( () => {
-    timeline.current = gsap.timeline({paused: true})
+
+  useEffect(() => {
+    timeline.current = gsap.timeline({ paused: true })
     timeline.current
-      .to(circle.current, {top: "-25%", width: "150%", duration: 0.4, ease: "power3.in"}, "enter")
-      .to(circle.current, {top: "-150%", width: "125%", duration: 0.25}, "exit")
+      .to(circle.current, { top: "-25%", width: "150%", duration: 0.4, ease: "power3.in" }, "enter")
+      .to(circle.current, { top: "-150%", width: "125%", duration: 0.25 }, "exit")
   }, [])
-  
+
   const manageMouseEnter = () => {
-    if(timeoutId) clearTimeout(timeoutId)
+    if (timeoutId) clearTimeout(timeoutId)
     timeline.current.tweenFromTo('enter', 'exit');
   }
 
   const manageMouseLeave = () => {
-    timeoutId = setTimeout( () => {
+    timeoutId = setTimeout(() => {
       timeline.current.play();
     }, 300)
   }
 
   return (
     <Magnetic>
-      <div className={styles.roundedButton} style={{overflow: "hidden"}} onMouseEnter={() => {manageMouseEnter()}} onMouseLeave={() => {manageMouseLeave()}} {...attributes}>
-          {
-            children
-          }
-        <div ref={circle} style={{backgroundColor}} className={styles.circle}></div>
+      <div className={styles.roundedButton} style={{ overflow: "hidden" }} onMouseEnter={manageMouseEnter} onMouseLeave={manageMouseLeave} {...attributes}>
+        {children}
+        <div ref={circle} style={{ backgroundColor }} className={styles.circle}></div>
       </div>
     </Magnetic>
   )
